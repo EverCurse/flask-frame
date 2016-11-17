201456f1472494c80b4e857642a9f6b8b1171487
sunqilin <sunqilin@cd.tuan800.com>
2016-11-17 15:03:02 +0800

    DATATABLE IN INDEX

--- app/views/main.py
+++ app/views/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+from flask import Response
+from flask import json
+
 main = Blueprint('main', __name__)
 from app.models.models import Users
 from flask_login import login_required
@@ -11,6 +14,20 @@ from flask_cas import login_required as c_login_required
 def index():
     return render_template('main.html')
 
+
+@main.route('/get_data')
+@login_required
+def datatable():
+    users=Users.query.all()
+    res=[]
+    for i in users:
+        res.append({
+            'username':i.username,
+            'passwd_hash':i.passwd_hash,
+            'last_login':i.last_login
+        })
+    res={'data':res}
+    return jsonify(res)
 
 @main.route('/bt')
 def bt():
